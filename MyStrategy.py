1dbe226c9c702a522f65afed1133cda678c352de
elsid <elsid.mail@gmail.com>
2015-11-21 02:01:18 +0300

    Updateable plots, tiles passability, more tests

--- MyStrategy.py
+++ MyStrategy.py
@@ -1,16 +1,35 @@
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from itertools import chain
-from numpy import array, dot
+from numpy import array, dot, array_equal
 from numpy.linalg import norm
 from scipy.sparse.csgraph import dijkstra
-from math import sqrt, pi
-# from matplotlib.pyplot import figure, ion, show
+from math import sqrt
+from matplotlib.pyplot import subplots, show, ion
 
 from model.Car import Car
 from model.Game import Game
 from model.Move import Move
 from model.World import World
 from model.TileType import TileType
+
+
+class MyStrategy:
+    def __init__(self):
+        self.__path_plots = PathPlots()
+        self.__tiles_barriers = defaultdict(lambda: defaultdict(lambda: None))
+
+    def move(self, me: Car, world: World, game: Game, move: Move):
+        matrix = AdjacencyMatrix(world.tiles_x_y)
+        tile = current_tile(me.x, me.y, game.track_tile_size)
+        tile_index = matrix.index(tile.x, tile.y)
+        path = list(path_to_end(tile_index, me.next_waypoint_index, matrix,
+                                world.waypoints))
+        path_tiles = (world.tiles_x_y[x][y] for x, y in path)
+        update_tiles_barriers(self.__tiles_barriers, path_tiles,
+                              game.track_tile_margin, game.track_tile_size)
+        move.engine_power = 0.5
+        print(me.x, me.y, tile.x, tile.y, tile_index, path[:2])
+        self.__path_plots.draw(path)
 
 
 Node = namedtuple('Node', ('x', 'y'))
@@ -102,7 +121,11 @@ class AdjacencyMatrix:
 
 
 def current_tile(x, y, track_tile_size):
-    return Point(int(x / track_tile_size - 0.5), int(y / track_tile_size - 0.5))
+    return Point(tile_coord(x, track_tile_size), tile_coord(y, track_tile_size))
+
+
+def tile_coord(value, track_tile_size):
+    return int(value / track_tile_size)
 
 
 def path_to_end(start_index, next_waypoint_index, matrix, waypoints):
@@ -124,7 +147,6 @@ def path_to_end(start_index, next_waypoint_index, matrix, waypoints):
             yield dst
             dst = predecessors.item(src, dst)
 
-    yield Point(*waypoints[next_waypoint_index - 1])
     for v in chain.from_iterable(generate()):
         yield Point(matrix.x_position(v), matrix.y_position(v))
 
@@ -161,29 +183,127 @@ def detail(path):
     return chain.from_iterable(replace(i, x) for i, x in enumerate(path))
 
 
-class MyStrategy:
-    def move(self, me: Car, world: World, game: Game, move: Move):
-        matrix = AdjacencyMatrix(world.tiles_x_y)
-        tile = current_tile(me.x, me.y, game.track_tile_size)
-        tile_index = matrix.index(tile.x, tile.y)
-        path = list(path_to_end(tile_index, me.next_waypoint_index, matrix,
-                                world.waypoints))
-        target = Point(*(array(path[1]) + array([0.5, 0.5]) *
-                         game.track_tile_size))
-        move.wheel_turn = me.get_angle_to(target.x, target.y) * 20.0 / pi
-        move.engine_power = 0.5
-        print('next_waypoint=', me.next_waypoint_index, me.next_waypoint_x,
-              me.next_waypoint_y, *world.waypoints[me.next_waypoint_index])
-        print('tile=', tile)
-        print('tile_index=', tile_index)
-        print('len(path)=', len(path))
-        print('target=', target)
-        # print((me.x, me.y), tuple(target))
-        # path_x = [x.x for x in path]
-        # path_y = [x.y for x in path]
-        # self.path.clear()
-        # self.path.set_xlim([min(path_x) - 0.5, max(path_x) + 0.5])
-        # self.path.set_ylim([min(path_y) - 0.5, max(path_y) + 0.5])
-        # self.path.plot(path_x, path_y, '-')
-        # self.path.plot(path_x, path_y, 'o')
-        # self.figure.canvas.draw()
+def update_tiles_barriers(values, tiles, tile_margin, tile_size):
+    for x, v in enumerate(tiles):
+        for y, tile_type in enumerate(v):
+            if values[x][y] is None:
+                values[x][y] = tile_barriers(tile_type, tile_margin, tile_size)
+
+
+def tile_barriers(tile_type, tile_margin, tile_size):
+    low = tile_margin
+    high = tile_size - low
+    left = Border([low, 0], [low, tile_size], [1, 0])
+    right = Border([high, 0], [high, tile_size], [-1, 0])
+    top = Border([0, low], [tile_size, low], [0, 1])
+    bottom = Border([0, high], [tile_size, high], [0, -1])
+    left_top = Circle([0, 0], tile_margin)
+    left_bottom = Circle([0, tile_size], tile_margin)
+    right_top = Circle([tile_size, 0], tile_margin)
+    right_bottom = Circle([tile_size, tile_size], tile_margin)
+    if tile_type == TileType.VERTICAL:
+        return left, right
+    elif tile_type == TileType.HORIZONTAL:
+        return top, bottom
+    elif tile_type == TileType.LEFT_TOP_CORNER:
+        return left, top, right_bottom
+    elif tile_type == TileType.RIGHT_TOP_CORNER:
+        return right, top, left_bottom
+    elif tile_type == TileType.LEFT_BOTTOM_CORNER:
+        return left, bottom, right_top
+    elif tile_type == TileType.RIGHT_BOTTOM_CORNER:
+        return right, bottom, left_top
+    elif tile_type == TileType.LEFT_HEADED_T:
+        return left_top, left_bottom, right
+    elif tile_type == TileType.RIGHT_HEADED_T:
+        return right_top, right_bottom, left
+    elif tile_type == TileType.TOP_HEADED_T:
+        return left_top, right_top, bottom
+    elif tile_type == TileType.BOTTOM_HEADED_T:
+        return left_bottom, right_bottom, top
+    elif tile_type == TileType.CROSSROADS:
+        return left_top, left_bottom, right_top, right_bottom
+    else:
+        return tuple()
+
+
+def tile_passability(barriers, height, width):
+    def impl(x, y):
+        return min(x.passability(x, y, height, width) for x in barriers)
+    return impl
+
+
+class Barrier:
+    def passability(self, x, y, height, width):
+        raise NotImplementedError()
+
+
+class Circle(Barrier):
+    def __init__(self, position, radius):
+        self.__position = array(position)
+        self.__radius = radius
+
+    def passability(self, x, y, height, width):
+        position = array((x, y))
+        radius = max((height, width))
+        return float(norm(self.__position - position) > self.__radius + radius)
+
+    def __repr__(self):
+        return 'Barrier(position={p}, radius={r})'.format(
+            p=repr(self.__position), r=repr(self.__radius))
+
+    def __eq__(self, other):
+        return (array_equal(self.__position, other.__position) and
+                self.__radius == other.__radius)
+
+
+class Border(Barrier):
+    def __init__(self, begin, end, normal):
+        self.__begin = array(begin)
+        self.__end = array(end)
+        self.__normal = array(normal)
+
+    def passability(self, x, y, height, width):
+        position = array((x, y))
+        to_car = position - self.__begin
+        to_end = self.__end - self.__begin
+        distance = sqrt(norm(to_car)**2 -
+                        (dot(to_car, to_end) / norm(to_end))**2)
+        if distance <= max((height, width)):
+            return 0.0
+        return float(dot(to_car, self.__normal) > 0)
+
+    def __repr__(self):
+        return 'Barrier(begin={b}, end={e}, normal={n})'.format(
+            b=repr(self.__begin), e=repr(self.__end), n=repr(self.__normal))
+
+    def __eq__(self, other):
+        return (array_equal(self.__begin, other.__begin) and
+                array_equal(self.__end, other.__end) and
+                array_equal(self.__normal, other.__normal))
+
+
+class PathPlots:
+    __plot_inited = False
+    __figure = None
+    __points_plot = None
+    __lines_plot = None
+
+    def draw(self, path):
+        path_x = array([p.x for p in path])
+        path_y = array([p.y for p in path])
+        if self.__plot_inited:
+            self.__points_plot.set_xdata(path_x)
+            self.__points_plot.set_ydata(path_y)
+            self.__lines_plot.set_xdata(path_x)
+            self.__lines_plot.set_ydata(path_y)
+            self.__figure.canvas.draw()
+        else:
+            self.__figure, axis = subplots()
+            axis.set_xlim([min(path_x) - 0.5, max(path_x) + 0.5])
+            axis.set_ylim([min(path_y) - 0.5, max(path_y) + 0.5])
+            self.__points_plot = axis.plot(path_x, path_y, 'o')[0]
+            self.__lines_plot = axis.plot(path_x, path_y, '-')[0]
+            ion()
+            show()
+            self.__plot_inited = True
--- test.py
+++ test.py
@@ -1,7 +1,18 @@
+from collections import defaultdict
 from unittest import TestCase, main
 from hamcrest import assert_that, equal_to
 from model.TileType import TileType
-from MyStrategy import AdjacencyMatrix, path_to_end, Point, is_direct
+from MyStrategy import (
+    AdjacencyMatrix,
+    path_to_end,
+    Point,
+    tile_coord,
+    current_tile,
+    Border,
+    Circle,
+    tile_barriers,
+    update_tiles_barriers,
+)
 
 
 class AdjacencyMatrixTest(TestCase):
@@ -140,14 +151,130 @@ class AdjacencyMatrixTest(TestCase):
 
 class PathToEndTest(TestCase):
     def test_from_vertical_to_next_vertical_returns_first_and_second_point(self):
+        start_index = 1
         next_waypoint_index = 1
         matrix = AdjacencyMatrix([
             [TileType.EMPTY, TileType.VERTICAL,
              TileType.VERTICAL, TileType.EMPTY],
         ])
         waypoints = [[0, 1], [0, 2]]
-        path = list(path_to_end(next_waypoint_index, matrix, waypoints))
-        assert_that(path, equal_to([Point(0, 1), Point(0, 2)]))
+        path = list(path_to_end(start_index, next_waypoint_index,
+                                matrix, waypoints))
+        assert_that(path, equal_to([Point(0, 2)]))
+
+    def test_over_three_vertical_returns_three_points(self):
+        start_index = 1
+        next_waypoint_index = 1
+        matrix = AdjacencyMatrix([
+            [TileType.EMPTY,
+             TileType.VERTICAL, TileType.VERTICAL, TileType.VERTICAL,
+             TileType.EMPTY],
+        ])
+        waypoints = [[0, 1], [0, 2], [0, 3]]
+        path = list(path_to_end(start_index, next_waypoint_index,
+                                matrix, waypoints))
+        assert_that(path, equal_to([Point(0, 2), Point(0, 3)]))
+
+
+class TileCoordTest(TestCase):
+    def test_at_0_with_tile_size_100_returns_0(self):
+        assert_that(tile_coord(0, 100), equal_to(0))
+
+    def test_at_99_with_tile_size_100_returns_0(self):
+        assert_that(tile_coord(99, 100), equal_to(0))
+
+    def test_at_100_with_tile_size_100_returns_1(self):
+        assert_that(tile_coord(100, 100), equal_to(1))
+
+
+class CurrentTileTest(TestCase):
+    def test_at_100_100_with_tile_size_100_returns_1_1(self):
+        assert_that(current_tile(100, 100, 100), equal_to(Point(1, 1)))
+
+
+class BorderTest(TestCase):
+    def test_passability_at_normal_side_with_fit_size_returns_1(self):
+        border = Border([0, 0], [1, 0], [0, 1])
+        assert_that(border.passability(0, 2, 1, 1), equal_to(1.0))
+
+    def test_passability_at_normal_side_with_unfit_size_returns_0(self):
+        border = Border([0, 0], [1, 0], [0, 1])
+        assert_that(border.passability(0, 2, 3, 3), equal_to(0.0))
+
+    def test_passability_at_not_normal_side_with_fit_size_returns_0(self):
+        border = Border([0, 0], [1, 0], [0, 1])
+        assert_that(border.passability(0, -2, 1, 1), equal_to(0.0))
+
+    def test_passability_at_not_normal_side_with_unfit_size_returns_0(self):
+        border = Border([0, 0], [1, 0], [0, 1])
+        assert_that(border.passability(0, -2, 3, 3), equal_to(0.0))
+
+
+class CircleTest(TestCase):
+    def test_passability_outside_radius_with_fit_size_returns_1(self):
+        circle = Circle([0, 0], 1)
+        assert_that(circle.passability(0, 3, 1, 1), equal_to(1.0))
+
+    def test_passability_inside_radius_with_fit_size_returns_0(self):
+        circle = Circle([0, 0], 1)
+        assert_that(circle.passability(0, 1, 1, 1), equal_to(0.0))
+
+    def test_passability_outside_radius_with_unfit_size_returns_0(self):
+        circle = Circle([0, 0], 1)
+        assert_that(circle.passability(0, 3, 2, 2), equal_to(0.0))
+
+    def test_passability_inside_radius_with_unfit_size_returns_0(self):
+        circle = Circle([0, 0], 4)
+        assert_that(circle.passability(0, 1, 4, 4), equal_to(0.0))
+
+
+class TileBarriersTest(TestCase):
+    def test_for_empty_returns_empty_tuple(self):
+        barriers = tile_barriers(TileType.EMPTY, 1, 3)
+        assert_that(barriers, equal_to(tuple()))
+
+    def test_for_vertical_returns_two_borders(self):
+        barriers = tile_barriers(TileType.VERTICAL, 1, 3)
+        assert_that(barriers, equal_to((
+            Border([1, 0], [1, 3], [1, 0]),
+            Border([2, 0], [2, 3], [-1, 0]),
+        )))
+
+    def test_for_horizontal_returns_two_borders(self):
+        barriers = tile_barriers(TileType.HORIZONTAL, 1, 3)
+        assert_that(barriers, equal_to((
+            Border([0, 1], [3, 1], [0, 1]),
+            Border([0, 2], [3, 2], [0, -1]),
+        )))
+
+    def test_for_left_top_corner_returns_two_borders_and_one_circle(self):
+        barriers = tile_barriers(TileType.LEFT_TOP_CORNER, 1, 3)
+        assert_that(barriers, equal_to((
+            Border([1, 0], [1, 3], [1, 0]),
+            Border([0, 1], [3, 1], [0, 1]),
+            Circle([3, 3], 1),
+        )))
+
+    def test_for_crossroads_returns_four_circles(self):
+        barriers = tile_barriers(TileType.CROSSROADS, 1, 3)
+        assert_that(barriers, equal_to((
+            Circle([0, 0], 1), Circle([0, 3], 1),
+            Circle([3, 0], 1), Circle([3, 3], 1),
+        )))
+
+
+class UpdateTilesBarriers(TestCase):
+    def test_with_one_empty_for_empty_should_fill(self):
+        tiles_barriers = defaultdict(lambda: defaultdict(lambda: None))
+        update_tiles_barriers(tiles_barriers, tiles=[[TileType.EMPTY]],
+                              tile_margin=1, tile_size=3)
+        assert_that(tiles_barriers, equal_to({0: {0: tuple()}}))
+
+    def test_with_different_tile_for_filled_should_do_not_change(self):
+        tiles_barriers = {0: {0: tuple()}}
+        update_tiles_barriers(tiles_barriers, tiles=[[TileType.VERTICAL]],
+                              tile_margin=1, tile_size=3)
+        assert_that(tiles_barriers, equal_to({0: {0: tuple()}}))
 
 
 if __name__ == '__main__':
