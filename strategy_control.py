7e731f378df78e5941c7fc2b636e2bdb6a639d40
elsid <elsid.mail@gmail.com>
2015-11-28 17:30:33 +0300

    Fix pid controller

--- strategy_control.py
+++ strategy_control.py
@@ -108,21 +108,23 @@ class Controller:
 
 
 class PidController:
+    __previous_error = 0
+    __integral = None
+
     def __init__(self, proportional_gain, integral_gain, derivative_gain):
         self.proportional_gain = proportional_gain
         self.integral_gain = integral_gain
         self.derivative_gain = derivative_gain
-        self.__previous_output = 0
-        self.__previous_error = 0
-        self.__integral = 0
 
     def __call__(self, error):
-        self.__integral += error
+        if self.__integral is None:
+            self.__integral = error
+        else:
+            self.__integral += error
         derivative = error - self.__previous_error
-        output = (self.proportional_gain * error +
-                  self.integral_gain * self.__integral +
-                  self.derivative_gain * derivative)
-        self.__previous_output = output
+        output = (error * self.proportional_gain +
+                  self.__integral * self.integral_gain +
+                  derivative * self.derivative_gain)
         self.__previous_error = error
         return output
 
