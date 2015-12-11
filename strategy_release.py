12ec0eaa8b0e7bcc31c4e6615a71a173a0fe09e6
elsid <elsid.mail@gmail.com>
2015-12-11 08:57:30 +0300

    Adjust curve using conditions

--- strategy_release.py
+++ strategy_release.py
@@ -740,7 +740,7 @@ class Course:
             )
         tile_size = context.game.track_tile_size
         sub_path = [context.position] + path
-        if reduce(mul, generate_cos(path), 1) < 0.9:
+        if reduce(mul, generate_cos(path), 1) < 0.8:
             target_position = Curve(sub_path).at(tile_size)
         else:
             target_position = max(
